620d1c3d252c3c66ec4a158be73646786624b722
David Harms <david.harms@iqmetrix.com>
2017-03-21 14:55:38 -0500

    Pushing new changes

--- source/utility/sorting/SortExtensions.cs
+++ source/utility/sorting/SortExtensions.cs
@@ -5,17 +5,29 @@ namespace code.utility.sorting
   public static class SortExtensions
   {
     public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer,
-  IGetTheValueOfAProperty<Item, Property> accessor)
-  where Property : IComparable<Property>
+      IGetTheValueOfAProperty<Item, Property> accessor)
+      where Property : IComparable<Property>
     {
       return then_by(previous_comparer, accessor, SortOrders.ascending);
     }
 
-    public static ICompareTwoItems<Item> then_by_descending<Item, Property>(this ICompareTwoItems<Item> previous_comparer,
-  IGetTheValueOfAProperty<Item, Property> accessor)
-  where Property : IComparable<Property>
+    public static ICompareTwoItems<Item> then_by_descending<Item, Property>(
+      this ICompareTwoItems<Item> previous_comparer,
+      IGetTheValueOfAProperty<Item, Property> accessor)
+      where Property : IComparable<Property>
     {
       return then_by(previous_comparer, accessor, SortOrders.descending);
+    }
+
+    public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer,
+      IGetTheValueOfAProperty<Item, Property> accessor, params Property[] sort_order)
+      where Property : IComparable<Property>
+    {
+      return (a, b) =>
+      {
+        var previous_result = previous_comparer(a, b);
+        return previous_result == 0 ? Sort<Item>.by(accessor, sort_order)(a, b) : previous_result;
+      };
     }
 
     public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer,
