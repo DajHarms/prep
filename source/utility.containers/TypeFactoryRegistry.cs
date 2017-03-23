6a5536764786b7ab6a1836fe690a062bf496511e
David Harms <david.harms@iqmetrix.com>
2017-03-23 16:51:45 -0500

    Pushing new changes

---
+++ source/utility.containers/TypeFactoryRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.utility.containers
+{
+  public class TypeFactoryRegistry : IFindFactoriesForAType
+  {
+    public ICreateOneDependency get_factory_that_can_create(Type item_to_create)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ source/utility.containers/TypeFactoryRegistrySpecs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using developwithpassion.specifications.assertions.interactions;
+using Machine.Specifications;
+using spec = developwithpassion.specifications.use_engine<Machine.Fakes.Adapters.Rhinomocks.RhinoFakeEngine>;
+
+namespace code.utility.containers
+{
+  [Subject(typeof(TypeFactoryRegistry))]
+  public class TypeFactoryRegistrySpecs
+  {
+    public abstract class concern : spec.observe<IFindFactoriesForAType, TypeFactoryRegistry>
+    {
+    }
+
+    public class when_i_have_the_factory_for_the_given_type : concern
+    {
+      Establish c = () =>
+      {
+        to_create = fake.an<ICreateOneDependency>();
+      };
+
+      Because b = () =>
+          result = sut.get_factory_that_can_create(typeof(Object));
+
+      private It returns_the_factory_that_matches_the_type = () =>
+          result.ShouldEqual(to_create);
+
+      static ICreateOneDependency result, to_create;
+    }
+  }
+}
